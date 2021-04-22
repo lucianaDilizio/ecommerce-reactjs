6cfa4ca07f043c6b6fa010ffeb727f514dbafc8f
lucianaDilizio <lgarcia@makingsense.com>
2021-04-22 10:36:38 -0300

    chore: add header and product list in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { ProductList } from './components/ProductList/ProductList';
 import './App.css';
+import { Header } from './components/Header/Header';
 
 function App() {
-  return <ProductList />;
+  return (
+    <>
+      <Header /> <ProductList />
+    </>
+  );
 }
 
 export default App;
