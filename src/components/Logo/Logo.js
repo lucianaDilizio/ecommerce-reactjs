788cd9466f7021df23604b395361f91731e1f7f5
lucianaDilizio <lgarcia@makingsense.com>
2021-05-03 17:32:42 -0300

    chore: create logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Logo.css';
+
+export const Logo = () => {
+  return (
+    <img
+      alt="logo"
+      src="https://freevector-images.s3.amazonaws.com/uploads/vector/preview/36682/36682.png"
+      className="logo"
+    />
+  );
+};
