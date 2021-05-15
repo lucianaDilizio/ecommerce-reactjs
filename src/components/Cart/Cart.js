baa906130bce45e0b8b1f41bb7c53003da34ed93
lucianaDilizio <lgarcia@makingsense.com>
2021-05-15 20:18:09 -0300

    chore: isolate CartProductList into a new file

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { NoProductsFound } from '../NoProductsFound/NoProductsFound';
+import React, { useState } from 'react';
 import { Popup } from '../Popup/Popup';
-import { Amount } from '../Product/Amount/Amount';
-import { updateAmount, removeFromCart } from '../actions';
 import './Cart.css';
+import CartProductList from './CartProductList';
+import { connect } from 'react-redux';
 
-const Cart = ({ cart, updateAmount, removeFromCart }) => {
+const Cart = ({ cart }) => {
   const [showPopup, setShowPopup] = useState(false);
 
   return (
@@ -19,75 +17,7 @@ const Cart = ({ cart, updateAmount, removeFromCart }) => {
       />
       {showPopup ? (
         <Popup handleShowPopup={setShowPopup}>
-          {cart.length ? (
-            <>
-              <div className="detail-container">
-                <table className="product-list">
-                  <thead className="product-list-header">
-                    <tr>
-                      <td>PRODUCT</td>
-                      <td>SUBTOTAL</td>
-                    </tr>
-                  </thead>
-                  <tbody className="product-list-body">
-                    {cart.map((product) => (
-                      <tr key={product.id}>
-                        <td>
-                          <table className="product-list-datail">
-                            <tbody>
-                              <tr>
-                                <td>{product.name}</td>
-                              </tr>
-                              <tr>
-                                <td>${product.price}</td>
-                              </tr>
-                              <tr>
-                                <td>
-                                  <Amount
-                                    handlerAmount={(amount) =>
-                                      updateAmount(product.id, amount)
-                                    }
-                                    defaultValue={1}
-                                    updatedValue={product.amount}
-                                  />
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </td>
-                        <td>
-                          {product.amount
-                            ? '$' + product.amount * product.price
-                            : ''}
-                        </td>
-                        <td>
-                          <span
-                            className="delete-icon"
-                            title="Delete item"
-                            onClick={() => removeFromCart(product.id)}
-                          >
-                            X
-                          </span>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-              <div className="total-container">
-                TOTAL: $
-                {cart
-                  .map((product) =>
-                    product.amount ? product.price * product.amount : 0,
-                  )
-                  .reduce(
-                    (acumulator, currentValue) => acumulator + currentValue,
-                  )}
-              </div>
-            </>
-          ) : (
-            <NoProductsFound />
-          )}
+          <CartProductList />
         </Popup>
       ) : (
         <></>
@@ -109,4 +39,4 @@ const mapStateToProps = (state) => {
   return { cart: state.cart };
 };
 
-export default connect(mapStateToProps, { updateAmount, removeFromCart })(Cart);
+export default connect(mapStateToProps)(Cart);
