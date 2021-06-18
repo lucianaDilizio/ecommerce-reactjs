f89a7281650d816f53694e6fd9e0ff7fe29b8561
lucianaDilizio <lgarcia@makingsense.com>
2021-06-18 14:30:32 -0300

    fix: delete not used parameter

--- src/components/actions/filtersActions.js
+++ src/components/actions/filtersActions.js
@@ -1,11 +1,10 @@
 export const UPDATE_FILTER = 'UPDATE_FILTER';
 
-export const updateFilter = (filter, type) => {
+export const updateFilter = (filter) => {
   return {
     type: UPDATE_FILTER,
     payload: {
       filter,
-      type,
     },
   };
 };
